7526274be2a1096cb76ad54c5c69d8e7af91dc88
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2023-07-29 23:10:18 -0300

    feat: first widget

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main () {
+  runApp(QuestionApp());
+}
+
+class QuestionApp extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello World'),
+    );
+  }
+}
