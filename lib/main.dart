3f9f7a0b156418cdcb8ff06c87727e350d3f5384
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2023-09-17 20:23:23 -0300

    refactor: map the answers

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import './question.dart';
 import './answer.dart';
 
-main () {
-  runApp(QuestionApp());
-}
+void main () => runApp(const QuestionApp());
 
 class _QuestionAppState extends State<QuestionApp> {
   var _selectedQuestion = 0;
@@ -17,21 +15,22 @@ class _QuestionAppState extends State<QuestionApp> {
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> questions = [
+    final questions = [
       {
         'text': 'Qual é a sua cor favorita?',
-        'answer': ['Preto','Vermelho','Verde','Branco',]
+        'answer': ['Preto','Vermelho','Verde','Branco']
       },
       {
         'text': 'Qual é a seu animal favorito?',
-        'answer': ['Coelho','Cobra','Elefante','Leão',]
+        'answer': ['Coelho','Cobra','Elefante','Leão']
       },
       {
         'text': 'Qual é sua estação favorita?',
-        'answer': ['Verão','Inverno','Outono','Primavera',]
+        'answer': ['Verão','Inverno','Outono','Primavera']
       }
     ];
 
+    List<String> answers = questions[_selectedQuestion]['answer'] as List<String>;
 
     return MaterialApp(
       home: Scaffold(
@@ -40,10 +39,8 @@ class _QuestionAppState extends State<QuestionApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_selectedQuestion]['text'].toString()),
-            Answer("Resposta 1", _answer),
-            Answer("Resposta 2", _answer),
-            Answer("Resposta 3", _answer),
+            Question(questions[_selectedQuestion]['text'] as String),
+            ...answers.map((text) => Answer(text, _answer)).toList()
           ],
         ),
       ),
